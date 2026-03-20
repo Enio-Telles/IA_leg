ec92615c22570fd3f1c1e5d14d9b760dca849457
Enio-Telles <eniotelles@gmail.com>
2026-03-20 14:16:28 -0400

    chore: remove transitional files and docs before merge

--- tests/test_safe_entrypoints.py
+++
@@ -1,8 +0,0 @@
-def test_safe_dashboard_entrypoint_imports():
-    import dashboard.app_safe  # noqa: F401
-
-
-def test_safe_pipeline_has_processar_tudo():
-    from etl.versionamento_pipeline_safe import processar_tudo
-
-    assert callable(processar_tudo)
